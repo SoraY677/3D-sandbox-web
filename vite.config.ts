bc98f26b1dce2d8b9ce50530938bee08eb19bb40
SoraY677 <SoraY677@outlook.com>
2022-11-03 20:47:48 +0900

    ルートパスを変更

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
 	build: {
-		outDir: 'examples'
+		outDir: 'examples',
 	},
+  base: '3D-sandbox-web/examples',
   resolve: {
     alias: {
       "@graphics/": `/src/script/graphics/`
